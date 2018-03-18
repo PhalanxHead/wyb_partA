59b1f0952dd4c995eaeff572494a5538289391cf
PhalanxHead <lhedt@hotmail.com>
2018-03-18 17:53:17 +1100

    Added is_valid_move()

--- partA_amy.py
+++ partA_amy.py
@@ -8,7 +8,7 @@ def prepare_board(board):
 		if char == "\n":
 			board_array.append(row)
 			row = []
-		
+
 		elif char != " ":
 			row.append(char)
 
@@ -31,48 +31,58 @@ def locations(board, player):
 
 	return location_array
 
+def is_valid_move(board, locations, piece, move):
+
+	move_i = move[0]
+	move_j = move[1]
+
+	piece_i = piece[0]
+	piece_j = piece[1]
+
+	try:
+	# try and move the piece, moves outside the board are invalid.
+		position = board[piece_i + move_i][piece_j + move_j]
+	except IndexError:
+		return False
+
+	# Outside the board again,
+	if ((piece_i + move_i) < 0) or ((piece_j + move_j) < 0):
+		return False
+
+	# One space moves are valid
+	if position == "-":
+		return True
+
+	#Try jumping
+	elif position == "X":
+		pass
+
+	elif position == "O" or position == "@":
+
+		try:
+			jump = board[piece_i + move_i*2][piece_j + move_j*2]
+		except IndexError:
+			return False
+
+		if ((piece_i + move_i*2) < 0) or ((piece_j + move_j*2) < 0):
+			return False
+
+		if jump == "-":
+			return True
+
+	return False
+
 """Determine the number of moves for each player """
 def moves(board, locations):
 	num_moves = 0
 	buffers = [(1,0), (0,1), (-1,0), (0,-1)]
-	
+
 	for piece in locations:
 		piece_moves = 0
 
-		init_i = piece[0]
-		init_j = piece[1]
+		for move in buffers:
 
-		for moveset in buffers:
-
-			buffer_i = moveset[0]
-			buffer_j = moveset[1]
-
-			try:
-				position = board[init_i + buffer_i][init_j + buffer_j]
-			except IndexError:
-				position = None
-
-			if ((init_i + buffer_i) < 0) or ((init_j + buffer_j) < 0):
-				position = None
-
-			if position == "-":
-				piece_moves += 1
-
-			elif position == "X":
-				pass
-
-			elif position == "O" or position == "@": 
-				
-				try:
-					jump = board[init_i + buffer_i*2][init_j + buffer_j*2]
-				except IndexError:
-					jump = None
-
-				if ((init_i + buffer_i*2) < 0) or ((init_j + buffer_j*2) < 0):
-					jump = None
-
-				if jump == "-":
-					piece_moves += 1
+			piece_moves += is_valid_move(board, locations, piece, move)
 
 		num_moves += piece_moves
 
@@ -82,7 +92,7 @@ def massacre(board, black, white):
 	sequence = []
 
 
-	
+
 
 	return sequence
 
@@ -109,7 +119,7 @@ white_locations = locations(board_as_array, "white")
 black_locations = locations(board_as_array, "black")
 
 """Based on what is given to us in stdin we either calculate the number of legal
-	moves for each of the players, or we perform MASSACRE!, finding sequence of 
+	moves for each of the players, or we perform MASSACRE!, finding sequence of
 	moves for white that will cleaar the board"""
 if command.lower() == "moves":
 	white_moves = moves(board_as_array, white_locations)
@@ -119,4 +129,3 @@ if command.lower() == "moves":
 elif command.lower() == "massacre":
 	#use massacre function
 	print("massacre block")
-
